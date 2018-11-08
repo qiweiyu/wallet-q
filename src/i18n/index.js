4bc1ff2a748c77f9fc4133a6c34123e59d1b8b0a
qiweiyu <qiweiyu@qtumdeMacBook-Pro.local>
2018-11-08 16:17:43 +0800

    fetch i18n from native

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,14 @@
+import { NativeModules, Platform } from 'react-native';
 import i18n from 'i18n-js';
 import en from './en';
 import zh from './zh';
+
+const locale = Platform.OS === 'ios' ? NativeModules.SettingsManager.settings.AppleLocale : NativeModules.I18nManager.localeIdentifier;
 
 i18n.fallbacks = true;
 i18n.translations = { en, zh };
 //todo add read config from device
 //i18n.locale = Localization.locale;
-i18n.locale = 'zh';
+i18n.locale = locale;
 
 export default i18n;
